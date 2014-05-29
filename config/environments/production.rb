70b8b4c47dd8e0a6cf430a046fbb30b89fc565c0
David Iorns <david.iorns@gmail.com>
2014-05-29 16:27:56 -0700

    Add sunspot config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  Sunspot.config.solr.url = ENV['WEBSOLR_URL']
 end
