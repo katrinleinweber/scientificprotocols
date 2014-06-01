b5e5129b86596e75abe427a6bbf5551e65b4f31a
David Iorns <david.iorns@gmail.com>
2014-06-01 11:33:36 -0700

    Revert test syntax

--- spec/controllers/protocols_controller_spec.rb
+++ spec/controllers/protocols_controller_spec.rb
@@ -5,37 +5,44 @@ describe ProtocolsController do
     let(:protocol) { FactoryGirl.create(:protocol) }
     describe 'GET #index' do
       it 'renders the :index template' do
-        expect{ get :index }.to render_template :index
+        get :index
+        expect(response).to render_template :index
       end
     end
     describe 'GET #show' do
       it 'renders the :show template' do
-        expect{ get :show, id: protocol.id }.to render_template :show
+        get :show, id: protocol.id
+        expect(response).to render_template :show
       end
     end
     describe 'GET #new' do
       it 'redirects to the login page' do
-        expect{ get :new }.to redirect_to new_user_session_url
+        get :new
+        expect(response).to redirect_to new_user_session_url
       end
     end
     describe 'POST #create' do
       it 'redirects to the login page' do
-        expect{ post :create, protocol: protocol }.to redirect_to new_user_session_url
+        post :create, protocol: FactoryGirl.attributes_for(:protocol)
+        expect(response).to redirect_to new_user_session_url
       end
     end
     describe 'GET #edit' do
       it 'redirects to the login page' do
-        expect{ get :edit, id: protocol.id }.to redirect_to new_user_session_url
+        get :edit, id: protocol.id
+        expect(response).to redirect_to new_user_session_url
       end
     end
     describe 'PATCH #update' do
       it 'redirects to the login page' do
-        expect{ patch :update, id: protocol.id, protocol: protocol }.to redirect_to new_user_session_url
+        patch :update, id: protocol.id, protocol: { title: Faker::Lorem.sentence(5) }
+        expect(response).to redirect_to new_user_session_url
       end
     end
     describe 'DELETE #destroy' do
       it 'redirects to the login page' do
-        expect{ delete :destroy, id: protocol.id }.to redirect_to new_user_session_url
+        delete :destroy, id: protocol.id
+        expect(response).to redirect_to new_user_session_url
       end
     end
   end
@@ -44,25 +51,25 @@ describe ProtocolsController do
     let(:protocol) { FactoryGirl.create(:protocol) }
     describe 'GET #index' do
       it 'renders the :index template' do
-        expect{ get :index }.to render_template :index
+        get :index
+        expect(response).to render_template :index
       end
     end
     describe 'GET #show' do
       it 'renders the :show template' do
-        expect{ get :show, id: protocol.id }.to render_template :show
+        get :show, id: protocol.id
+        expect(response).to render_template :show
       end
     end
     describe 'GET #new' do
       it 'renders the :new template' do
-        expect{ get :new }.to render_template :new
+        get :new
+        expect(response).to render_template :new
       end
     end
     describe 'POST #create' do
-      it 'renders the :show template' do
-        expect{ post :create, protocol: protocol }.to redirect_to protocol
-      end
       it 'creates a new protocol' do
-        expect{ post :create, protocol: protocol }.to change{ Protocol.count }.from(0).to(1)
+        expect{ post :create, protocol: FactoryGirl.attributes_for(:protocol) }.to change{ Protocol.count }.by(1)
       end
     end
     describe 'GET #edit' do
@@ -72,7 +79,9 @@ describe ProtocolsController do
     end
     describe 'PATCH #update' do
       it 'forbids access' do
-        expect{ patch :update, id: protocol.id, protocol: protocol }.to raise_error(CanCan::AccessDenied)
+        expect{
+          patch :update, id: protocol.id, protocol: { title: Faker::Lorem.sentence(5) }
+        }.to raise_error(CanCan::AccessDenied)
       end
     end
     describe 'DELETE #destroy' do
@@ -87,49 +96,55 @@ describe ProtocolsController do
     let(:protocol) { protocol_manager.protocol }
     describe 'GET #index' do
       it 'renders the :index template' do
-        expect{ get :index }.to render_template :index
+        get :index
+        expect(response).to render_template :index
       end
     end
     describe 'GET #show' do
       it 'renders the :show template' do
-        expect{ get :show, id: protocol.id }.to render_template :show
+        get :show, id: protocol.id
+        expect(response).to render_template :show
       end
     end
     describe 'GET #new' do
       it 'renders the :new template' do
-        expect{ get :new }.to render_template :new
+        get :new
+        expect(response).to render_template :new
       end
     end
     describe 'POST #create' do
-      it 'redirects to the show page' do
-        expect{ post :create, protocol: protocol }.to redirect_to protocol
-      end
       it 'creates a new protocol' do
-        expect{ post :create, protocol: protocol }.to change{ Protocol.count }.from(0).to(1)
+        expect{ post :create, protocol: FactoryGirl.attributes_for(:protocol) }.to change{ Protocol.count }.by(1)
       end
     end
     describe 'GET #edit' do
       it 'renders the :edit template' do
-        expect{ get :edit, id: protocol.id }.to render_template :edit
+        get :edit, id: protocol.id
+        expect(response).to render_template :edit
       end
     end
     describe 'PATCH #update' do
       it 'redirects to the show page' do
-        expect{ patch :update, id: protocol.id, protocol: protocol }.to redirect_to protocol
+        patch :update, id: protocol.id, protocol: { title: Faker::Lorem.sentence(5) }
+        expect(response).to redirect_to protocol
       end
       it 'updates a protocol' do
         new_title = Faker::Lorem.sentence 5
-        patch :update, id: protocol.id, protocol: attributes_for(:protocol, title: new_title)
+        patch :update, id: protocol.id, protocol: { title: new_title }
         protocol.reload
         expect(protocol.title).to eq(new_title)
       end
     end
     describe 'DELETE #destroy' do
       it 'redirects to the protocols page' do
-        expect{ delete :destroy, id: protocol.id }.to redirect_to protocols_url
+        delete :destroy, id: protocol.id
+        expect(response).to redirect_to protocols_url
       end
       it 'deletes a protocol' do
-        expect{ delete :destroy, id: protocol.id }.to change{ Protocol.count }.from(1).to(0)
+        new_protocol = FactoryGirl.create(:protocol_manager, user: @current_user).protocol
+        expect{
+          delete :destroy, id: new_protocol.id
+        }.to change{ Protocol.count }.by(-1)
       end
     end
   end
