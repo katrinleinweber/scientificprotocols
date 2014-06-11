41759fe0ed8bdcc3ed6d8f4d36d59d09dd425044
David Iorns <david.iorns@gmail.com>
2014-06-10 23:12:12 -0700

    Fix sitemap redirect

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
   resources :protocols
+  get 'sitemap.xml', to: redirect('https://s3.amazonaws.com/scientificprotocols/sitemaps/sitemap.xml.gz')
   get ':action' => 'static#:action'
-  get 'sitemap.xml', to: redirect('http://scientificprotocols.s3-website-us-east-1.amazonaws.com/sitemaps/sitemap.xml.gz')
 end
