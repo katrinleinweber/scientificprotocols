773bcf930fad33683e6e20268c8da5bffc45f72b
David Iorns <david.iorns@gmail.com>
2014-07-11 21:26:24 -0700

    Add referrer logging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,7 @@ class ApplicationController < ActionController::Base
     query_string = nil
     begin
       query_string = URI.parse(request.referer).query
+      Rails.logger.info("Query string: #{query_string}")
     rescue URI::InvalidURIError => e
       Rails.logger.warn("Could not parse #{request.referer}\n#{e.to_s}")
     end
