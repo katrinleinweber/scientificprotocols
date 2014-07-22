60aefaf19d5939071944ddd5c155003429d5d36f
Dmitry Golyshev <dmitry@scienceexchange.com>
2014-07-22 11:52:28 -0700

    internal server error handler

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::BaseController < ActionController::Base
   respond_to :json
 
+  rescue_from Exception, with: :internal_server_error
 
   def is_integer_id(id)
     begin
@@ -11,8 +12,20 @@ class Api::V1::BaseController < ActionController::Base
     end
   end
 
-  # TODO(golyshev) Add default rendering fo the 404, 500, 403 pages
+  # TODO(golyshev) Add default rendering fo the 404, 403 pages
 
   protected :is_integer_id
+
+  def internal_server_error(exception = nil)
+    result = {error: 'internal_server_error'}
+    result.merge!({debug_message: exception.message}) if exception
+    if exception
+      logger.error exception.message
+      logger.error exception.backtrace.join("\n")
+    end
+    respond_with(result, status: :internal_server_error)
+  end
+
+  protected :internal_server_error
 
 end
