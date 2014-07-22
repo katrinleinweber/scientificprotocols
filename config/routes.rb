0fcb2d3deea207384ccf8c96d35e1a25107208ef
Michael Kompanets <mkompanets@gmail.com>
2014-07-22 12:10:10 -0700

    adding omniauthable to user.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount_roboto
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'home#index'
   resources :protocols do
     collection do
