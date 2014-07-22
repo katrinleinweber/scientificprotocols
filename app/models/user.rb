c7f66e9f85eba27e8499b093ff55dbe2d3567509
Michael Kompanets <mkompanets@gmail.com>
2014-07-22 12:14:11 -0700

    user self from omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,4 +20,13 @@ class User < ActiveRecord::Base
       where(conditions).first
     end
   end
+
+  def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name   # assuming the user model has a name
+      user.image = auth.info.image # assuming the user model has an image
+    end
+  end
 end
