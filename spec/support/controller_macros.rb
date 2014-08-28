805a7c6455c6475b950ec47cafa6da022bd3fc9c
David Iorns <david.iorns@gmail.com>
2014-08-27 19:39:56 -0700

    Add comment spec

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,10 +1,10 @@
 module ControllerMacros
-  def login_user
+  def login_user(access_token = Rails.configuration.api_github)
     before(:each) do
       @current_user = FactoryGirl.create(:user)
       session[:user_id] = @current_user.id
       request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:github]
-      session[:access_token] = Rails.configuration.api_github
+      session[:access_token] = access_token
     end
   end
 end
