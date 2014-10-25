801770cd10ec6678798d9ec4c4ea7fce9b77fce1
David Iorns <david.iorns@gmail.com>
2014-10-25 14:54:03 -0700

    Implement better user profile page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper'
 
 describe UsersController do
+  let(:username) { 'dior001' }
   context 'Guest Access' do
-    let(:user) { FactoryGirl.create(:user) }
+    let(:user) { FactoryGirl.create(:user, username: username) }
     describe 'GET #show' do
       it 'renders the :show template' do
         get :show, id: user.id
@@ -12,7 +13,7 @@ describe UsersController do
   end
   context 'Authenticated User' do
     login_user
-    let(:user) { FactoryGirl.create(:user) }
+    let(:user) { FactoryGirl.create(:user, username: username) }
     describe 'GET #show' do
       it 'renders the :show template' do
         get :show, id: user.id
