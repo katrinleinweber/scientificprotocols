c24a1cd6830d540df9137057c75abf7b30c90365
David Iorns <david.iorns@gmail.com>
2015-01-09 23:16:00 -0800

    Add tag category migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150104222554) do
+ActiveRecord::Schema.define(version: 20150110044817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
