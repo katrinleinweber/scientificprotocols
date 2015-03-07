6579f7dca6559cdc442249313fe43e3e9dbaa71e
David Iorns <david.iorns@gmail.com>
2015-03-07 10:34:49 -0800

    Reduce auth scopes

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -2,21 +2,21 @@ case Rails.env
   when 'development'
     PROTOCOL_FILE_NAME = 'protocol.md'
     GITHUB_AUTH_PATH = '/auth/github'
-    GITHUB_AUTH_SCOPES = 'user,repo,gist,write:repo_hook,read:org'
+    GITHUB_AUTH_SCOPES = 'user:email,gist'
     GITHUB_MAX_PAGE_SIZE = 100
   when 'test'
     PROTOCOL_FILE_NAME = 'protocol.md'
     GITHUB_AUTH_PATH = '/auth/github'
-    GITHUB_AUTH_SCOPES = 'user,repo,gist,write:repo_hook,read:org'
+    GITHUB_AUTH_SCOPES = 'user:email,gist'
     GITHUB_MAX_PAGE_SIZE = 100
   when 'staging'
     PROTOCOL_FILE_NAME = 'protocol.md'
     GITHUB_AUTH_PATH = '/auth/github'
-    GITHUB_AUTH_SCOPES = 'user,repo,gist,write:repo_hook,read:org'
+    GITHUB_AUTH_SCOPES = 'user:email,gist'
     GITHUB_MAX_PAGE_SIZE = 100
   when 'production'
     PROTOCOL_FILE_NAME = 'protocol.md'
     GITHUB_AUTH_PATH = '/auth/github'
-    GITHUB_AUTH_SCOPES = 'user,repo,gist,write:repo_hook,read:org'
+    GITHUB_AUTH_SCOPES = 'user:email,gist'
     GITHUB_MAX_PAGE_SIZE = 100
 end
